c3989212786c2a33491e89069da12fa15eb7e169
Steven <pangzi1271@gmail.com>
2021-06-06 18:00:19 +1000

    commit on 20210606

--- 10_Cookie/src/TwoServlet.java
+++ 10_Cookie/src/TwoServlet.java
@@ -1,7 +1,9 @@
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
+import java.awt.print.PrinterGraphics;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class TwoServlet extends HttpServlet {
     @Override
@@ -11,21 +13,57 @@ public class TwoServlet extends HttpServlet {
         int noodle = 15;
         int rice = 12;
         Cookie[] cookieArray = null;
-        String userName;
-        int money;
+        String userName = null;
+        int money = 0;
+        PrintWriter out = null;
+        out = response.getWriter();
 
+        //1. 读取请求头的信息，获取用户点餐信息
         String order = request.getParameter("food");
 
+        //2. 获取cookie中的信息
         cookieArray = request.getCookies();
 
+        //配置向网面输出的字符类型及字符编码
+        response.setContentType("text/html;charset=utf-8");
 
         for (Cookie card:cookieArray){
             String key = card.getName();
             String value = card.getValue();
             if ("userName".equals(key)){
+                //获取用户名
                 userName = value;
             }else if("money".equals(key)){
+                //获取当前用户的金额
                 money = Integer.valueOf(value);
+                if ("饺子".equals(order)){
+                    if(money < dumpling) {
+                        out.print("<font style='color:red;font-size:25px'>您的余额不足，请充值。</font>");
+                    }else {
+                        out.print("<font>用户：" + userName + "点餐：" + order + "消费：" + dumpling + "</font>");
+                    }
+                }
+                if ("包子".equals(order)){
+                    if(money < meatBun) {
+                        out.print("<font style='color:red;font-size:25px'>您的余额不足，请充值。</font>");
+                    }else {
+                        out.print("<font>用户：" + userName + "点餐：" + order + "消费：" + meatBun + "</font>");
+                    }
+                }
+                if ("牛肉面".equals(order)){
+                    if(money < noodle) {
+                        out.print("<font style='color:red;font-size:25px'>您的余额不足，请充值。</font>");
+                    }else {
+                        out.print("<font>用户：" + userName + "点餐：" + order + "消费：" + noodle + "</font>");
+                    }
+                }
+                if ("盖饭".equals(order)){
+                    if(money < rice) {
+                        out.print("<font style='color:red;font-size:25px'>您的余额不足，请充值。</font>");
+                    }else {
+                        out.print("<font>用户：" + userName + "，点餐：" + order + "，消费：" + rice + "元</font>");
+                    }
+                }
             }
         }
     }
